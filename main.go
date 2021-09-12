f652e0e7c0240ea680d71baa7dd00e1af29326da
unknown <deepakumar50136@gmail.com>
2021-09-13 01:11:46 +0530

    added time

--- main.go
+++ main.go
@@ -17,4 +17,13 @@ func main() {
 
 	// if i want week day with time then pass "Monday" "15:04:05" respectively
 	fmt.Println(PresentTime.Format("01-02-2006  15:040:05 Monday"))
+
+	// create date from manual input by using time.date(year, month, hiur, minute,second, nano sec, timezone) method
+
+	// here time.Utc
+
+	CreateDate := time.Date(2020, time.January, 1, 18, 18, 0, 0, time.UTC)
+
+	fmt.Println(CreateDate)
+	fmt.Println(CreateDate.Format("01-02-2006 15:04:05 Monday"))
 }
