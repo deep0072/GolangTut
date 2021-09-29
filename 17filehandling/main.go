3a897d3ebb2fb56f9e537aca303718aa42690ee7
unknown <deepakumar50136@gmail.com>
2021-09-29 23:18:39 +0530

    here file creationg

--- 17filehandling/main.go
+++ 17filehandling/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
 	fmt.Println("ho welcome to files handling")
+
+	content := " hi check the text file" //this is content that will be in file
+
+	file, err := os.Create("./myfile.txt") // create text file
+
+	if err != nil {
+		panic(err) //it will check error during creation of file
+		// panic keyword ==>  just shut down the execution of program and show the error
+
+	}
+
+	length, err := io.WriteString(file, content) //here "io" package will write the content into file
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("length is :", length)
+
+	defer file.Close() // here file should need to be close after intialisation. and here i used defer to execute it in the end
 }
