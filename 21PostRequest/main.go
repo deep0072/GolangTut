b9322603ceaf001384cadfc1306d02cead20aa83
unknown <deepakumar50136@gmail.com>
2021-10-05 20:05:31 +0530

    post request

--- 21PostRequest/main.go
+++ 21PostRequest/main.go
@@ -15,20 +15,20 @@ func main() {
 func PerformPostUrl() {
 	const myUrl = "http://localhost:8000/post"
 
-	requestBody := strings.NewReader(`
+	requestBody := strings.NewReader(`  
 	{
 		"coursename": "Golang",
 		"Price": 0,
 		"Platform": "Youtube"
-	}`)
+	}`) //creat content as json
 
-	response, err := http.Post(myUrl, "application/json", requestBody)
+	response, err := http.Post(myUrl, "application/json", requestBody) //use .Post method takes 3 param url, type of content, content
 	if err != nil {
 		panic(err)
 	}
 	defer response.Body.Close()
 
-	content, err := ioutil.ReadAll(response.Body)
+	content, err := ioutil.ReadAll(response.Body) //get the response
 	if err != nil {
 		panic(err)
 	}
