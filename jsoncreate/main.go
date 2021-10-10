7e62aad8d90db8e54e24768a88f01569dd2f16a4
unknown <deepakumar50136@gmail.com>
2021-10-10 22:11:05 +0530

    json encoding and decoding

--- jsoncreate/main.go
+++ jsoncreate/main.go
@@ -18,6 +18,7 @@ func main() {
 	fmt.Println("welcome to json data creation")
 
 	jsonEncode()
+	DecodeJson()
 }
 
 func jsonEncode() {
@@ -31,14 +32,18 @@ func jsonEncode() {
 		{"js", 660, "Deepcourse.com", "deep123", []string{"web-dev", "js"}},
 	}
 
-	// finalJson, err := json.Marshal(DeepCourse)
+	finalJson, err := json.Marshal(DeepCourse)
+
 	//It allows you to do encoding of JSON data as defined in the RFC 7159.
 	// When working with data we
 	//usually want to encode some Go struct into a json string
+	if err != nil {
+		panic(err)
+	}
 
 	// to print json in reading manner we use "json.MarshalIndent"
 
-	finalJson, err := json.MarshalIndent(DeepCourse, "", "\t")
+	finalJson1, err := json.MarshalIndent(DeepCourse, "", "\t")
 	//here second parameter is prefix \t is tab will be given in json output
 
 	if err != nil {
@@ -46,5 +51,47 @@ func jsonEncode() {
 	}
 
 	fmt.Println(string(finalJson))
+	fmt.Println(string(finalJson1))
+
+}
+
+func DecodeJson() {
+
+	JsonDataFromweb := []byte(`
+	{
+		"courseName": "Ruby",
+
+		"Price": 560,
+
+		"Platform": "Deepcourse.com",
+
+		"Tags": ["web-dev",	"ruby"]
+	
+				
+	}
+	
+	
+	
+	`)
+
+	var DeepCourse courses
+
+	//now check json is valid
+
+	checkValid := json.Valid(JsonDataFromweb)
+
+	if checkValid {
+		println("json is valid")
+
+		json.Unmarshal(JsonDataFromweb, &DeepCourse)
+		//here json data will be decode and store in to &Deepcourse
+
+		fmt.Println(DeepCourse) //print the encoded json data
+
+	} else {
+		println("json was not valid")
+	}
+
+	//now decode tha json using Unmarshal
 
 }
